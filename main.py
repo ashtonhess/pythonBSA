700bdee3333230ef6b9d4a718b0b0b53dd757153
ashtonhess <ashton.hess@yahoo.com>
2022-01-30 20:32:58 -0600

    function skeletons

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+import time
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def doBSA(filename, number):
+    return 0
 
+def algBSA(array, number, lowPtr, highPtr):
+    return -1
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
