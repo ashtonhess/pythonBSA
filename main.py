0c73b85dc2bf93480b7201b9839c1056f9adb553
ashtonhess <ashton.hess@yahoo.com>
2022-01-30 20:33:54 -0600

    comments and algBSA func complete

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ import time
 def doBSA(filename, number):
     return 0
 
+#Helper function to directly implement BSA alg.
+#Split this off from the doBSA so alg. implementation is in a focused func.
 def algBSA(array, number, lowPtr, highPtr):
-    return -1
+    if highPtr >= lowPtr:
+        midPtr = lowPtr+(highPtr-lowPtr)//2
+        # // -> floor division
+        if int(array[midPtr]) == number:
+            return midPtr
+        elif int(array[midPtr]) > number:
+            return algBSA(array, number, lowPtr, midPtr-1)
+        else:
+            return algBSA(array, number, midPtr+1, highPtr)
+    else:
+        return -1
 
