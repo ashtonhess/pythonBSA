f1c452b2d4aa587f48e712781ce9abbd2b805062
ashtonhess <ashton.hess@yahoo.com>
2022-01-30 22:56:23 -0600

    Importing numpy and matplotlib for visual graphs. Added arrays to store the algo_times and all_times. Graph implementation is left

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import time
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rcParams['figure.figsize'] = (12.0, 9.0)
+
+algo_times = []
+all_times = []
 
 def doBSA(filename, number):
     all_timeStart = time.time()
@@ -12,7 +18,9 @@ def doBSA(filename, number):
 
     algo_timeStart = time.time()
     result = algBSA(dataContentList, number, 0, len(dataContentList) - 1)
-    print("algo_time: %s seconds" % (time.time() - algo_timeStart))
+    algo_times.append(time.time() - algo_timeStart)
+    print("algo_time (printed): %s seconds" % (time.time() - algo_timeStart))
+    #print is approximate because it is calculated again. Times stored in array and graphed are more accurate than this print right now.
 
     if result != -1:
         print("Element is at index: " + str(result))
@@ -35,6 +43,21 @@ def algBSA(array, number, lowPtr, highPtr):
             return algBSA(array, number, midPtr+1, highPtr)
     else:
         return -1
+
+def fillAllTimes():
+    all_times.append(doBSA("data1.txt", 19))
+    all_times.append(doBSA("data1.txt", 225))
+    all_times.append(doBSA("data1.txt", 705))
+    all_times.append(doBSA("data2.txt", 128))
+    all_times.append(doBSA("data2.txt", 5756))
+    all_times.append(doBSA("data2.txt", 9982))
+    all_times.append(doBSA("data3.txt", 1997))
+    all_times.append(doBSA("data3.txt", 20680))
+    all_times.append(doBSA("data3.txt", 23887))
+    all_times.append(doBSA("data4.txt", 68189))
+    all_times.append(doBSA("data4.txt", 921111))
+    all_times.append(doBSA("data4.txt", 935099))
+    return
 
 print("\t\t__________data1.txt__________")
 print("\n")
@@ -75,3 +98,19 @@ print("\n")
 print("all_time: " + str(doBSA("data4.txt", 935099)))
 
 print("\n\n")
+
+X = [2,2,2,3,3,3,4,4,4,5,5,5]
+
+print(algo_times)
+plt.scatter(X, algo_times)
+plt.show()
+
+print("\n\n\n")
+
+fillAllTimes()
+print(all_times)
+
+algo_timesMean = np.mean(algo_times)
+all_timesMean = np.mean(all_times)
+
+
